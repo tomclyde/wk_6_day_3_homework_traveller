fbd56eec4b1d9046eddccbde6c98d8af984dfa09
Tom C <tclyde@hotmail.com>
2019-01-30 20:42:43 +0000

    complete

---
+++ models/traveller.js
@@ -0,0 +1,47 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+    return this.journeys.map((journey) => {
+      return journey.startLocation;
+    });
+
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+   return this.journeys.filter((journey) => {
+     return journey.transport === transport;
+   });
+};
+
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+    return this.journeys.reduce((total, journey) => {
+      return total += journey.distance;
+    }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  let journeyList = [];
+  journeyList = this.journeys.map((journey) => {
+     return journey.transport;
+    });
+  let uniqueJourneyList = Array.from(new Set(journeyList));
+  return uniqueJourneyList;
+};
+
+
+module.exports = Traveller;
